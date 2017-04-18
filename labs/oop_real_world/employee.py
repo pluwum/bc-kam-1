1498964a8fbe7faea054e01f99d92952d77c6e29
Patrick Luwum <luwyxx@gmail.com>
2017-04-18 17:27:33 +0300

    Added A simple OOP example with inheritance

---
+++ labs/oop_real_world/employee.py
@@ -0,0 +1,27 @@
+class Person(object):
+
+   def __init__(self,name, age):
+      self.age = age
+      self.name = name
+   
+   def setAge(age):
+      self.age = age   
+
+class Employee(Person):
+
+   def __init__(self, name, age, salary, department):
+      Person.__init__(self,name,age)
+      self.salary = salary
+      self.department = department
+   
+   def getSalary(self):
+      print ("Name : ", self.name,  ", Salary: ", self.salary)
+
+   def getAge(self):
+      print ("Name : ", self.name,  ", Age: ", self.age)
+
+
+#patrick = Employee('Patrick','26','455433','IT')
+
+#patrick.getAge()
+
